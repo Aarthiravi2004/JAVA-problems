6b63ae3aaea164a7f8af3f09a04bf45c6c2839c1
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-09-11 12:41:34 +0530

    Create PRIME.java

---
+++ PRIME.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class Prime{
+    Public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int n= sc.nextInt();
+      System.out.print(Isprime(n));
+    }
+  static boolean Isprime(int n){
+    int c=2;
+    if(n<=1){
+      return false;
+    }
+    while(c*c <= n){    //avoiding the repeated multiplcation eg: 8*2 == 2*8
+      if(n%c==0){
+        return false;
+      }
+      c++;
+    }
+    retur true;
+  }
+}
+      
