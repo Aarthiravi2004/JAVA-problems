3ddeb8cb78aab7a8b37c66a969ea0548c967dbff
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-10-24 22:14:20 +0530

    Create Pattern-3.java

---
+++ PatternProblems/Pattern-3.java
@@ -0,0 +1,46 @@
+import java.lang.*;
+class Main{
+    public static void main(String[] args)
+    {
+       int n = 4;
+       for(int i=1;i<=n*2;i++)
+       {
+           int number;
+           if(i>n)
+           {
+               number = (n*2)-i+2;
+           }
+           else
+           {
+               number = i;
+           }
+           for(int space=0;space <= n-number;space++)
+           {
+               System.out.print(" ");
+           }
+           
+           for(int j=number;j>=1;j--)
+           {
+               System.out.print(j+" ");
+           }
+           for(int k=2;k<=number;k++)
+           {
+               System.out.print(k+" ");
+           }
+       }
+       System.out.println();
+    }
+}
+
+
+
+OUTPUT:
+N = 4
+      1
+    2 1 2
+  3 2 1 2 3
+4 3 2 1 2 3 4
+  3 2 1 2 3 
+    2 1 2
+      1
+      
