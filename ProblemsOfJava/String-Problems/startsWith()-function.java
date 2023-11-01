6087f871fad56114ae88c6dbd18c6f7f3f92fc62
Aarthi <135338637+Aarthiravi2004@users.noreply.github.com>
2023-11-01 13:48:12 +0530

    Update startsWith()-function.java

--- ProblemsOfJava/String-Problems/startsWith()-function.java
+++ ProblemsOfJava/String-Problems/startsWith()-function.java
@@ -1,4 +1,4 @@
-class HelloWorld {
+1.class HelloWorld {
     public static void main(String[] args) {
         String str ="appbcdapplegha";
         String copy = "";
@@ -23,3 +23,33 @@ class HelloWorld {
 
 OUTPUT:
 appbcdgha
+
+
+
+2.NOT startsWith() function example
+
+    class HelloWorld {
+    public static void main(String[] args) {
+        String str ="appbcdapplegha";
+        String copy = "";
+        System.out.print(recursion(str));
+    }
+    public static String recursion(String str)
+    {
+        if(str.isEmpty())
+        {
+            return "";
+        }
+        if(str.startsWith("app") && !str.startsWith("apple"))
+        {
+            return recursion(str.substring(5));
+        }
+        
+           return str.charAt(0) + recursion(str.substring(1));
+        
+        
+    }
+}
+
+OUTPUT:
+dapplegha
